89784a030e019ec4e92b2a1a259a2387fcc5d4fd
Leandro Poblet <lpoblet@msa.com.ar>
2014-02-19 17:06:57 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+function mostrar () {
+    main.style.display = 'none';
+    inventory.style.display = 'inline-block';
+}
+
+function admSelectCheck(nameSelect)
+{
+    if(nameSelect){
+        admOptionValue = nameSelect.value;
+        if(admOptionValue != 0){
+            quantity.style.display = "";
+            quantity.options.length = 0;
+            switch (admOptionValue) {
+                case "1":
+                    for ( i = 1; i <= 5; i += 1 ) {
+                        option = document.createElement( 'option' );
+                        option.value = option.text = i;
+                        quantity.add( option );
+                    }
+                break;
+                case "2":
+                    for ( i = 1; i <= 3; i += 1 ) {
+                        option = document.createElement( 'option' );
+                        option.value = option.text = i;
+                        quantity.add( option );
+                    }
+                break;
+                case "3":
+                    for ( i = 1; i <= 2; i += 1 ) {
+                        option = document.createElement( 'option' );
+                        option.value = option.text = i;
+                        quantity.add( option );
+                    }
+                break;
+                case "4":
+                    for ( i = 1; i <= 1; i += 1 ) {
+                        option = document.createElement( 'option' );
+                        option.value = option.text = i;
+                        quantity.add( option );
+                    }
+                break;
+            }
+        }
+        else {
+            quantity.style.display = "none";
+        }
+    }
+    else{
+        quantity.style.display = "none";
+    }
+}
+
+function enable (nameSelect) {
+    admOptionValue = nameSelect.value;
+    if (admOptionValue != 0) {
+        document.getElementById("enviar").disabled = false;
+    } else {
+        document.getElementById("enviar").disabled = true;
+    };
+}
---
+++ test.py
@@ -0,0 +1,16 @@
+import pygtk
+pygtk.require('2.0')
+import os
+import urllib
+
+from zaguan import Zaguan
+
+def load_window():
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    file_ = os.path.join(cur_dir, 'index.html')
+    uri = 'file://' + urllib.pathname2url(file_)
+    zaguan = Zaguan(uri)
+    zaguan.run()
+
+if __name__ == "__main__":
+    load_window()
