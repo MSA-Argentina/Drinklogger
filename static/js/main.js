7ec73c6a349352fd756eaa9ba9383198ce7742ba
Leandro Poblet <lpoblet@msa.com.ar>
2014-02-20 15:45:08 -0300

    Soporte de javascript sin ser muy "javascriptoso"

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 function mostrar () {
-    main.style.display = 'none';
-    inventory.style.display = 'inline-block';
+    main.style.display = "none";
+    inventory.style.display = "inline-block";
 }
 
 var firstCheck = false; // Work in Progress
@@ -9,39 +9,8 @@ function admSelectCheck(nameSelect)
 {
     if(nameSelect){
         admOptionValue = nameSelect.value;
-        if(admOptionValue != 0){
-            quantity.style.display = "";
-            quantity.options.length = 0;
-            switch (admOptionValue) {
-                case "1":
-                    for ( i = 1; i <= 5; i += 1 ) {
-                        option = document.createElement( 'option' );
-                        option.value = option.text = i;
-                        quantity.add( option );
-                    }
-                break;
-                case "2":
-                    for ( i = 1; i <= 3; i += 1 ) {
-                        option = document.createElement( 'option' );
-                        option.value = option.text = i;
-                        quantity.add( option );
-                    }
-                break;
-                case "3":
-                    for ( i = 1; i <= 2; i += 1 ) {
-                        option = document.createElement( 'option' );
-                        option.value = option.text = i;
-                        quantity.add( option );
-                    }
-                break;
-                case "4":
-                    for ( i = 1; i <= 1; i += 1 ) {
-                        option = document.createElement( 'option' );
-                        option.value = option.text = i;
-                        quantity.add( option );
-                    }
-                break;
-            }
+        if(admOptionValue != "null"){
+            quantity.style.display = "inline-block";
             };
         }
         else {
