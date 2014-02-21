76f9aad4894fc49f48af54c98509a64f9b3f614b
Leandro Poblet <lpoblet@msa.com.ar>
2014-02-21 17:42:55 -0300

    Soporte parcial (Muy parcial) de consultas semanales)

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ auth = Auth(app, db)
 
 class Producto(db.Model):
     nombre = CharField()
+    precio = FloatField()
     desc = TextField()
     cant = IntegerField()
 
@@ -58,6 +59,7 @@ class Usuario(db.Model):
 class Consumo(db.Model):
     usuario = ForeignKeyField(Usuario)
     producto = ForeignKeyField(Producto)
+    precio = FloatField()
     cantidad = IntegerField()
     fecha = DateTimeField(default=datetime.datetime.now)
 
@@ -70,7 +72,7 @@ class Consumo(db.Model):
 
 # Admin
 class ProductoAdmin(ModelAdmin):
-    columns = ("nombre", "cant",)
+    columns = ("nombre", "precio", "cant",)
 
 
 class ConsumoAdmin(ModelAdmin):
@@ -91,15 +93,17 @@ admin.setup()
 def home():
     productos = Producto.select()
     usuarios = Usuario.select()
+    consumo = Consumo.select().limit(5)
     exito = None
     return render_template("index.html", productos=productos,
-                           usuarios=usuarios, exito=exito)
+                           usuarios=usuarios, exito=exito,)
 
 
 @app.route("/mandar", methods=["POST", "GET"])
 def consumo():
     productos = Producto.select()
     usuarios = Usuario.select()
+    consumo = Consumo.select()
     if (request.form["productos"] != "null"):
         cantidad_actual = Producto\
             .get(Producto.id == request.form["productos"]).cant
@@ -108,6 +112,8 @@ def consumo():
             consumo = Consumo()
             consumo.usuario = request.form["personas"]
             consumo.producto = request.form["productos"]
+            consumo.precio = Producto.select()\
+                .where(Producto.id == request.form["productos"]).get().precio
             consumo.cantidad = request.form["cantidad"]
             consumo.save()
             actualizar_cantidad = Producto.update(cant=cantidad_nueva)\
@@ -115,17 +121,31 @@ def consumo():
             actualizar_cantidad.execute()
             exito = True
             return render_template("index.html", productos=productos,
-                                   usuarios=usuarios, exito=exito)
+                                   usuarios=usuarios, exito=exito,)
         else:
             exito = False
             return render_template("index.html", productos=productos,
-                                   usuarios=usuarios, exito=exito)
+                                   usuarios=usuarios, exito=exito,)
     else:
         exito = False
         return render_template("index.html", productos=productos,
-                               usuarios=usuarios, exito=exito)
+                               usuarios=usuarios, exito=exito,)
 
+@app.route("/consulta/", methods=["POST"])
+def consulta():
+    if (request.form["fecha"] != ""):
+        import datetime
+        ahora = str(datetime.datetime\
+                .strftime(datetime.datetime.now(), "%Y-%m-%d"))
+        fecha = str(datetime.datetime\
+                .strptime(request.form["fecha"], "%Y-%m-%d").date())
+        consumo_semanal = Consumo.select()\
+            .where(Consumo.fecha == fecha)
+        return render_template("consultas.html", consumo=consumo_semanal)
 
 # Principal
 if __name__ == '__main__':
+    Usuario.create_table(fail_silently=True)
+    Producto.create_table(fail_silently=True)
+    Consumo.create_table(fail_silently=True)
     app.run()
