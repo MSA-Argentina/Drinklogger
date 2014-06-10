b064e0cf7885f2ef9040e921f4783c0bfabc6f79
Felipe Lerena <felipelerena@gmail.com>
2014-06-10 14:31:08 -0300

    refactorizando pepochizando y poniendole amor

--- config.py
+++ config.py
@@ -1,27 +1,24 @@
+import logging
 import os
-from flask import Flask
-from flask_peewee.db import Database
-from flask_peewee.auth import Auth
-from flask_peewee.rest import RestAPI
-from flask.ext.sendmail import Mail
+
 
 # Configuracion
 DATABASE = {
-    'name': '',
-    'engine': '',
-    'user': '',
+    'name': 'drinklogger',
+    'engine': 'peewee.PostgresqlDatabase',
+    'user': 'felipe',
+    'password': '',
 }
 
-DEBUG = False # Activar fuera del repo
-SECRET_KEY = '' # Agregar fuera del repo
+DEBUG = True # Activar fuera del repo
+LOG_LEVEL = logging.DEBUG
+SECRET_KEY = 'change me' # Agregar fuera del repo
+LOG_NAME = "drinklogger.log"
 
 # Evita errores al traer templates en distintos ambientes
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'template')
+TEMPLATES_DIR= os.path.join(os.path.dirname(os.path.abspath(__file__)), 'template')
 
-app = Flask(__name__, template_folder=tmpl_dir)
-app.config.from_object(__name__)
-
-db = Database(app)
-auth = Auth(app, db)
-api = RestAPI(app)
-mail = Mail(app)
+try:
+    import config_local
+except ImportError:
+    pass
---
+++ create_db.py
@@ -0,0 +1,16 @@
+from application import auth
+from database import Usuario, Producto, Consumo
+
+
+def main():
+    Usuario.create_table(fail_silently=True)
+    Producto.create_table(fail_silently=True)
+    Consumo.create_table(fail_silently=True)
+    auth.User.create_table(fail_silently=True)  # make sure table created.
+    admin = auth.User(username='admin', email='', admin=True, active=True)
+    admin.set_password('admin')
+    admin.save()
+
+
+if __name__ == '__main__':
+    main()
